b52a28f12aab3567be3e8c97d116a95e1958ce6d
shivam49037940 <sforshivam7940@gmail.com>
2025-03-05 14:20:14 +0530

    fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 
     <html lang="en">
       <head>
-      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link rel="icon" href="/favicon.ico" />
       </head>
       <body className={inter.className}>
         <Navbar />
